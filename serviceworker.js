efb342a04b81650fb98434b9dda5e1fc52867bd5
Lunga Mthayi <lunga.mthayi@metropolitan.co.za>
2020-06-11 20:01:50 +0200

    first commit

---
+++ serviceworker.js
@@ -0,0 +1,36 @@
+
+var cacheName = "sw-v1";
+var filesToCache = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./images/a.jpg",
+  "./images/b.jpg",
+  "./images/a.jpg"
+];
+
+self.addEventListener("install", function (e) {
+  console.log("<ServiceWorker> ---- Install v1");
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      console.log("<ServiceWorker> --- Caching app shell");
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
